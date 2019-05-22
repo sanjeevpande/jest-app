f2833f79a84dcdd27e0d0edb66e7a5cdb99abca9
Sanjeev Pande <sanjeev.pande@getzephyr.com>
2019-05-22 09:05:39 +0530

    app change

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//import DropdownMenu, { DropdownItemGroup, DropdownItem } from '@atlaskit/dropdown-menu';
+
+function onItemClick(arg) {
+  console.log('keydown');
+}
+
+var obj = {
+  dragHandleProps: {
+    onMouseDown: function(arg) {
+      console.log('clicked', arg.target);
+    }
+  }
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<DropdownMenu
+        trigger="Choices"
+        triggerType="button"
+        shouldFlip={false}
+        position="right middle"
+      >
+        <DropdownItem 
+          dnd={Object.assign({}, obj, {dragHandleProps: { ...obj.dragHandleProps, value: 'Sydney'}})}
+          onClick={onItemClick.bind(this, 'Sydney')}
+        >Sydney</DropdownItem>
+        <DropdownItem 
+          dnd={obj}
+          onClick={onItemClick.bind(this, 'Bangalore')}
+        >Bangalore</DropdownItem>
+      </DropdownMenu>*/}
     </div>
   );
 }
